8db00ca0b306e5e437dcd6b6615de70a356aee3f
cmadurei <cmadurei@gmail.com>
2021-12-28 23:31:44 -0300

    Primeiro commit - teste IntelliJ

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        int a =2;
+        int b =1;
+        System.out.println("Palmeiras ganhou " + (a+b) + " vezes a Libertadores! \n");
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
